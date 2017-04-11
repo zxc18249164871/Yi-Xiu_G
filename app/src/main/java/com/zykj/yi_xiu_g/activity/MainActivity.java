1f53ef51db6797d4c38b6ebb991236d0cf931847
zxc18249164871 <18249164871@163.com>
2017-04-11 16:29:01 +0800

    编辑 MainActivity  添加点击事件

--- app/src/main/java/com/zykj/yi_xiu_g/activity/MainActivity.java
+++ app/src/main/java/com/zykj/yi_xiu_g/activity/MainActivity.java
@@ -1,10 +1,64 @@
 package com.zykj.yi_xiu_g.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zykj.yi_xiu_g.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by zykj on 2017/4/8.
  */
 
 public class MainActivity extends Activity {
+    @Bind(R.id.haerbin)
+    LinearLayout haerbin;
+    @Bind(R.id.gerenzhongxin)
+    ImageView gerenzhongxin;
+    @Bind(R.id.imageView2)
+    ImageView imageView2;
+    @Bind(R.id.imageView3)
+    ImageView imageView3;
+    @Bind(R.id.tianzhuan_shoujiweixiu)
+    FrameLayout tianzhuanShoujiweixiu;
+    @Bind(R.id.tiaozhuan_diannaoweixiu)
+    FrameLayout tiaozhuanDiannaoweixiu;
+    @Bind(R.id.textView2)
+    TextView textView2;
+    @Bind(R.id.imageView)
+    ImageView imageView;
+    @Bind(R.id.tiaozhuan_jiadianweixiu)
+    FrameLayout tiaozhuanJiadianweixiu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.haerbin, R.id.gerenzhongxin, R.id.tianzhuan_shoujiweixiu, R.id.tiaozhuan_diannaoweixiu, R.id.tiaozhuan_jiadianweixiu})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.haerbin:
+                break;
+            case R.id.gerenzhongxin:
+                break;
+            case R.id.tianzhuan_shoujiweixiu:
+                break;
+            case R.id.tiaozhuan_diannaoweixiu:
+                break;
+            case R.id.tiaozhuan_jiadianweixiu:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/zykj/yi_xiu_g/activity/MyTopBar.java
+++ app/src/main/java/com/zykj/yi_xiu_g/activity/MyTopBar.java
@@ -16,10 +16,20 @@ import com.zykj.yi_xiu_g.R;
 
 public class MyTopBar extends RelativeLayout {
     //    左面按钮  LeftText
-
+//    <attr name="LeftText"></attr>
+//    <attr name="LefrtTextSize" format="string"></attr>
+//    <attr name="LefrtTextGB" format="color|reference"></attr>
+//    <!--中间标题-->
+//    <attr name="TitleBar"></attr>
+//    <attr name="TitleTextSize" format="string"></attr>
+//    <attr name="TitleTextGB" format="color|reference"></attr>
+//    <!--右面按钮-->
+//    <attr name="RightText"></attr>
+//    <attr name="RightTextSize" format="string"></attr>
+//    <attr name="RightTextGB" format="color|reference"></attr>
     private String LeftText;
-    private float LefrtTextSize;
-    private Drawable RightTextGB;
+    private float LeftTextSize;
+    private Drawable LeftTextGB;
     //    中间标题  TitleBar
     private String TitleBar;
     private float TitleTextSize;
@@ -27,7 +37,7 @@ public class MyTopBar extends RelativeLayout {
     //  右面按钮 RightText
     private String RightText;
     private float RightTextSize;
-    private Drawable LefrtTextGB;
+    private Drawable RightTextGB;
     private Context context;
     private TextView left, title, right;
 
@@ -44,8 +54,8 @@ public class MyTopBar extends RelativeLayout {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyTopBar);
 //        获取左面按钮的属性
         LeftText = typedArray.getString(R.styleable.MyTopBar_LeftText);
-        LefrtTextSize = typedArray.getFloat(R.styleable.MyTopBar_LefrtTextSize, 0);
-        LefrtTextGB = typedArray.getDrawable(R.styleable.MyTopBar_LefrtTextGB);
+        LeftTextSize = typedArray.getFloat(R.styleable.MyTopBar_LefrtTextSize, 0);
+        LeftTextGB = typedArray.getDrawable(R.styleable.MyTopBar_LefrtTextGB);
 //        获取标题属性
         TitleBar = typedArray.getString(R.styleable.MyTopBar_TitleBar);
         TitleTextSize = typedArray.getFloat(R.styleable.MyTopBar_TitleTextSize, 0);
@@ -64,8 +74,8 @@ public class MyTopBar extends RelativeLayout {
 //        把控件属性设置到对应空间上
 //        把左面控件属性设置到左面控件上
         left.setText(LeftText);
-        left.setTextSize(LefrtTextSize);
-        left.setBackgroundDrawable(LefrtTextGB);
+        left.setTextSize(LeftTextSize);
+        left.setBackgroundDrawable(LeftTextGB);
 //        把标题内容控件属性设置标题上
         title.setText(TitleBar);
         title.setTextSize(TitleTextSize);
@@ -78,13 +88,15 @@ public class MyTopBar extends RelativeLayout {
 //        把左侧的控件添加到View中
         LayoutParams leftparams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         leftparams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        addView(left, leftparams);
 //        把中间的标题添加到View中
         LayoutParams titleparams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         titleparams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        addView(title, titleparams);
 //        把右侧的控件添加到View中
         LayoutParams rightparams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         rightparams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
+        addView(right, rightparams);
 
     }
 
